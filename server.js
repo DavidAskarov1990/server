6bdc9c7caa537be6a122ac7d9297a482b784c40b
DavidAskarov1990 <happy@gmail.com>
2017-05-19 08:16:33 +0300

    fix

--- server.js
+++ server.js
@@ -4,9 +4,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var fs = require('fs');
 var request = require('request');
 var config = require('./config');
+var path = require('path');
 
 var app = express();
 var port = process.env.PORT || config.port;
@@ -17,24 +17,22 @@ app.use(cookieParser());
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'text/html', 'content-type, application/json, Authorization, set-cookie, Accept, X-Requested-With');
-    if ('OPTIONS' === req.method) {
-        res.send(200);
-    } else {
-        next();
-    }
+    res.header('Access-Control-Allow-Headers', 'content-type, application/json, Authorization, set-cookie, Accept, X-Requested-With');
+    next();
 });
 
-app.use('/', function (req, res) {
-    var x = request('https://www.youtube.com/watch?v=haoTFLjysjk');
-    req.pipe(x);
-    x.pipe(res)
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + "/index.html"))
 });
 
-app.use(express.static('public'));
+// app.use('/', function (req, res) {
+//     var x = request('https://www.youtube.com/watch?v=haoTFLjysjk');
+//     req.pipe(x);
+//     x.pipe(res)
+// });
+
 app.use(require('./server/authorisation/index'));
 
 var server = app.listen(port, function() {
-    console.log('Server up and running in %d ', server.address().port);
+    console.log('Server up and running in %d ', server.address().port)
 });
